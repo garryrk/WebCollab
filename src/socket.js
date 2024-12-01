9c87b27b0f599363c273ae523137c8f8046c2b29
garryrk <rokhum18sep@gmail.com>
2024-12-02 01:12:28 +0530

    Code Update

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,6 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io(process.env.REACT_APP_BACKEND_URL, options);
+    console.log("chekcing backend url", process.env.REACT_APP_BACKEND_URL)
+    return io("http://localhost:5000", options);
 };
